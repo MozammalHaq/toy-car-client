501239bc2605ab3b76b7fee2880bf6ee8a645cc4
MozammalHaq <mrmozammal@gmail.com>
2023-05-18 00:17:00 +0600

    basic route setting ok

---
+++ src/assets/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+]);
+
+export default router;
