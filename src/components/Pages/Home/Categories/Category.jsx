496947d7990946f432b164455b55e48b0c0b747f
MozammalHaq <mrmozammal@gmail.com>
2023-05-19 01:13:04 +0600

    added react tostify

--- src/components/Pages/Home/Categories/Category.jsx
+++ src/components/Pages/Home/Categories/Category.jsx
@@ -1,9 +1,19 @@
+import { useContext } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContext } from '../../../AuthProvider/AuthProvider';
 
+const Category = ({ product }) => {
+    const { id, name, price, rating, imageSrc } = product;
+    const { user } = useContext(AuthContext);
 
-const Category = ({product}) => {
-    const {id, name, price, rating, imageSrc} = product;
+    const viewMessage = () => {
+        { !user && toast("Please, First Login"); }
+    }
+
     return (
         <div className="card bg-base-100 shadow-xl overflow-hidden">
+            <ToastContainer />
             <figure className="p-4"><img src={imageSrc} alt="Shoes" /></figure>
             <div className="card-body bg-blue-100">
                 <h2 className="card-title">{name}</h2>
@@ -12,7 +22,7 @@ const Category = ({product}) => {
                     <span className="font-bold text-primary">Tk. {price}</span>
                 </div>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">View Details</button>
+                    <button onClick={viewMessage} className="btn btn-primary">View Details</button>
                 </div>
             </div>
         </div>
